7ec81de5af00e8f873def81fdad78225e1221a9e
Eder Soares <edersoares@me.com>
2024-05-07 20:42:16 -0300

    Use model directly

--- tests/UseCase/HasBelongsToTest.php
+++ tests/UseCase/HasBelongsToTest.php
@@ -5,11 +5,10 @@ declare(strict_types=1);
 use Dex\Laravel\Anything\Models\Anything;
 use Workbench\Dex\Laravel\Anything\App\Models\Category;
 use Workbench\Dex\Laravel\Anything\App\Models\Post;
-use Workbench\Dex\Laravel\Anything\Database\Factories\PostFactory;
 use function Pest\Laravel\assertDatabaseCount;
 
 test('post has belongs to category', function () {
-    PostFactory::new()->create();
+    Post::factory()->create();
 
     assertDatabaseCount(Post::class, 1);
     assertDatabaseCount(Category::class, 0);
