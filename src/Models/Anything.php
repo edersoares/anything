7a2a2a171028fc9d672f5e91a279d501139a570a
Eder Soares <edersoares@me.com>
2024-05-08 09:57:12 -0300

    Docs

--- src/Models/Anything.php
+++ src/Models/Anything.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * @property string $label
  * @property string $slug
+ * @property string $group
  */
 class Anything extends Model
 {
--- workbench/app/Models/Post.php
+++ workbench/app/Models/Post.php
@@ -5,6 +5,10 @@ namespace Workbench\Dex\Laravel\Anything\App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $category_id
+ * @property string $title
+ */
 class Post extends Model
 {
     use HasFactory;
