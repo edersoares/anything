8e7ed26c6875bd634193346d8ce8e3b4e933d7d5
Eder Soares <edersoares@me.com>
2024-05-08 14:35:14 -0300

    Search

--- src/Http/Controllers/AnythingController.php
+++ src/Http/Controllers/AnythingController.php
@@ -5,11 +5,20 @@ declare(strict_types=1);
 namespace Dex\Laravel\Anything\Http\Controllers;
 
 use Dex\Laravel\Anything\Models\Anything;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Request;
 
 class AnythingController
 {
-    public function __invoke()
+    /**
+     * @return Collection<int, Anything>
+     */
+    public function __invoke(Request $request): Collection
     {
-        return Anything::query()->get();
+        return Anything::query()
+            ->when($search = $request->query('search'), function ($query) use ($search) {
+                $query->where('slug', 'like', "%{$search}%");
+            })
+            ->get();
     }
 }
--- tests/Feature/FetchAnythingTest.php
+++ tests/Feature/FetchAnythingTest.php
@@ -16,6 +16,22 @@ test('fetch all available anything', function () {
         ->assertJson($anything->toArray());
 });
 
+test('fetch filtered available anything', function () {
+    $this->seed(GenderSeeder::class);
+
+    $male = Gender::get('male');
+    $female = Gender::get('female');
+
+    $this->get('/anything?search=female')
+        ->assertOk()
+        ->assertJson([
+            $female->toArray(),
+        ])
+        ->assertJsonMissing([
+            $male->toArray(),
+        ]);
+});
+
 test('fetch some type', function () {
     $this->seed(GenderSeeder::class);
     $this->seed(RaceSeeder::class);
