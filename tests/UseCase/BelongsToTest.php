8d262e9b2601b3da634f161d58ab1d6103ccfb4a
Eder Soares <edersoares@me.com>
2024-05-08 14:27:18 -0300

    Remove fixture

--- tests/UseCase/BelongsToTest.php
+++ tests/UseCase/BelongsToTest.php
@@ -9,42 +9,16 @@ use function Pest\Laravel\assertDatabaseCount;
 
 use Workbench\Dex\Laravel\Anything\App\Models\Gender;
 use Workbench\Dex\Laravel\Anything\App\Models\Person;
-use Workbench\Dex\Laravel\Anything\App\Models\Post;
 use Workbench\Dex\Laravel\Anything\App\Models\Race;
 use Workbench\Dex\Laravel\Anything\Database\Factories\PersonFactory;
 use Workbench\Dex\Laravel\Anything\Database\Seeders\GenderSeeder;
 use Workbench\Dex\Laravel\Anything\Database\Seeders\RaceSeeder;
 
 test('post has belongs to category', function () {
-    Post::factory()->create();
+    PersonFactory::new()->create();
 
-    assertDatabaseCount(Post::class, 1);
-    assertDatabaseCount(Anything::class, 1);
-});
-
-test('anything using belongs relation', function () {
-    $red = Anything::factory()->create(['label' => 'Red']);
-    $green = Anything::factory()->create(['label' => 'Green']);
-    $blue = Anything::factory()->create(['label' => 'Blue']);
-
-    Post::factory()->create([
-        'category_id' => $green,
-    ]);
-
-    assertDatabaseCount(Post::class, 1);
-    assertDatabaseCount(Anything::class, 3);
-
-    $count = Post::query()
-        ->where('category_id', $green->getKey())
-        ->count();
-
-    expect($count)->toBe(1);
-
-    $count = Post::query()
-        ->whereIn('category_id', [$red->getKey(), $blue->getKey()])
-        ->count();
-
-    expect($count)->toBe(0);
+    assertDatabaseCount(Person::class, 1);
+    assertDatabaseCount(Anything::class, 2);
 });
 
 test('ensure anything extended models do query correcly', function () {
